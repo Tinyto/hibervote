c84baf408cb6e001abd37ed791e7fba9a8f231fb
Angel <Angel@DESKTOP-0NG6AT6>
2016-02-22 12:03:23 +0100

    input="date"

--- src/java/DAO/Operaciones.java
+++ src/java/DAO/Operaciones.java
@@ -6,23 +6,42 @@
 package DAO;
 
 import POJO.Persona;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-/**
- *
- * @author Angel
- */
+
 public class Operaciones {
     
-    
+    /**
+     *
+     * @author Tiny
+     */
     public int Alta(Persona p,SessionFactory con){
         
         return 0;
     }
-    
+    /**
+     *
+     * @author Angel
+     */
     public int Baja(Persona p,SessionFactory con){
         
-        return 0;
+       Session ses = con.openSession();
+        Transaction tx = ses.beginTransaction();
+        try{
+        Query q = ses.createQuery("FROM Persona WHERE dni= :nif");
+        q.setParameter("nif", p.getDni());
+        Persona e=(Persona)q.uniqueResult();
+        ses.delete(e);
+        }catch(Exception e){
+            
+            tx.rollback();
+            return -1;
+        }
+        tx.commit();
+        return 1;
     }
     
 }
