bd1e64723222c9c7afabc4ccedac941c2ace788a
Angel <Angel@DESKTOP-0NG6AT6>
2016-02-22 13:05:11 +0100

    servlet listarpersona

---
+++ src/java/BLL/Listarpersona.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL;
+
+import DAO.NewHibernateUtil;
+import DAO.Operaciones;
+import POJO.Persona;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author Angel
+ */
+public class Listarpersona extends HttpServlet {
+    private SessionFactory con;
+    @Override
+    public void init() throws ServletException {
+        con= NewHibernateUtil.getSessionFactory();
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession(true);
+        Operaciones op=new Operaciones();
+        Iterator it=op.getGente();
+        Persona p;
+        
+        try (PrintWriter out = response.getWriter()) {
+            
+         while(it.hasNext()){
+         
+             p=(Persona)it.next();
+             
+             out.print("<h3>PERSONA "+p.getId()+"</h3>");
+             out.print("<p>"+p.getNombre()+" "+p.getApellido()+"</p>");
+             out.print("<p> Dirección: "+p.getDireccion().getCalle()+", Piso:"+p.getDireccion().getPiso()+",Portal:"+p.getDireccion().getPortal()+"</p>");
+             
+         
+         }
+           
+            
+            
+          out.print("<a href='index.jsp'><button value='Volver a inicio'></a>");
+           
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
